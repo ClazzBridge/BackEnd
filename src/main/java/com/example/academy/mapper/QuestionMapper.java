c3bd874da5bc4b7056468aae6d6a11ed0eaa171c
ftfg777 <ftfgz5093@gmail.com>
2024-10-05 14:35:57 +0900

    .

--- src/main/java/com/example/academy/mapper/QuestionMapper.java
+++ src/main/java/com/example/academy/mapper/QuestionMapper.java
@@ -1,5 +1,6 @@
 package com.example.academy.mapper;
 
+import com.example.academy.domain.Member;
 import com.example.academy.domain.Question;
 import com.example.academy.domain.User;
 import com.example.academy.dto.QuestionCreateDTO;
@@ -12,26 +13,26 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface QuestionMapper {
 
-  QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
+    QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
 
-  // Domain to DTO
-  @Mapping(source = "user.id", target = "userId")
-  QuestionCreateDTO questionToQuestionCreateDTO(Question question);
+    // Domain to DTO
+    @Mapping(source = "user.id", target = "userId")
+    QuestionCreateDTO questionToQuestionCreateDTO(Question question);
 
-  @Mapping(source = "user.name", target = "userName")
-  @Mapping(source = "question.id", target = "id")
-  @Mapping(source = "question.solved", target = "isSolved")
-  @Mapping(source = "question.recommended", target = "isRecommended")
-  QuestionReadDTO questionToQuestionReadDTO(Question question, User user);
+    @Mapping(source = "user.name", target = "userName")
+    @Mapping(source = "question.id", target = "id")
+    @Mapping(source = "question.solved", target = "isSolved")
+    @Mapping(source = "question.recommended", target = "isRecommended")
+    QuestionReadDTO questionToQuestionReadDTO(Question question, Member user);
 
-  @Mapping(source = "question.id", target = "id")
-  QuestionUpdateDTO questionToQuestionUpdateDTO(Question question);
+    @Mapping(source = "question.id", target = "id")
+    QuestionUpdateDTO questionToQuestionUpdateDTO(Question question);
 
-  // DTO do Domain
-  @Mapping(target = "id", ignore = true)      // id는 자동 생성되므로 무시
-  @Mapping(target = "solved", ignore = true)
-  @Mapping(target = "recommended", ignore = true)
-  @Mapping(target = "createDate", ignore = true)
-  @Mapping(source = "user", target = "user")
-  Question questionCreateDTOToQuestion(QuestionCreateDTO questionCreateDTO, User user);
+    // DTO do Domain
+    @Mapping(target = "id", ignore = true)      // id는 자동 생성되므로 무시
+    @Mapping(target = "solved", ignore = true)
+    @Mapping(target = "recommended", ignore = true)
+    @Mapping(target = "createDate", ignore = true)
+    @Mapping(source = "user", target = "user")
+    Question questionCreateDTOToQuestion(QuestionCreateDTO questionCreateDTO, Member user);
 }
--- src/main/java/com/example/academy/service/QuestionService.java
+++ src/main/java/com/example/academy/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.academy.service;
 
+import com.example.academy.domain.Member;
 import com.example.academy.domain.Question;
 import com.example.academy.domain.User;
 import com.example.academy.dto.QuestionCreateDTO;
@@ -21,70 +22,73 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class QuestionService {
 
-  private final QuestionRepository questionRepository;
-  private final UserRepository userRepository;
-  private final QuestionMapper questionMapper = QuestionMapper.INSTANCE;
+    private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
+    private final QuestionMapper questionMapper = QuestionMapper.INSTANCE;
 
-  @Autowired
-  public QuestionService(QuestionRepository questionRepository, UserRepository userRepository) {
-    this.questionRepository = questionRepository;
-    this.userRepository = userRepository;
-  }
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository, UserRepository userRepository) {
+        this.questionRepository = questionRepository;
+        this.userRepository = userRepository;
+    }
 
-  public List<QuestionReadDTO> getAllQuestions() {
-    return questionRepository.findAllQuestionReadDTOs();
-  }
+    public List<QuestionReadDTO> getAllQuestions() {
+        return questionRepository.findAllQuestionReadDTOs();
+    }
 
-  public QuestionReadDTO getQuestionById(Long id) {
-    Question question = questionRepository.findById(id).orElse(null);
-    return questionMapper.questionToQuestionReadDTO(question, question.getUser());
-  }
+    public QuestionReadDTO getQuestionById(Long id) {
+        Question question = questionRepository.findById(id).orElse(null);
+        return questionMapper.questionToQuestionReadDTO(question, question.get());
+    }
 
-  public QuestionReadDTO createQuestion(QuestionCreateDTO questionCreateDTO) {
-    User user = userRepository.findById(questionCreateDTO.getUserId())
-        .orElseThrow(() -> new RuntimeException("User not found"));
-    Question newQuestion = questionMapper.questionCreateDTOToQuestion(questionCreateDTO, user);
-    Question savedQuestion = questionRepository.save(newQuestion);
+    public QuestionReadDTO createQuestion(QuestionCreateDTO questionCreateDTO) {
+        Member user = userRepository.findById(questionCreateDTO.getUserId())
+            .orElseThrow(() -> new RuntimeException("User not found"));
+        Question newQuestion = questionMapper.questionCreateDTOToQuestion(questionCreateDTO, user);
+        Question savedQuestion = questionRepository.save(newQuestion);
 
-    return questionMapper.questionToQuestionReadDTO(savedQuestion, user);
-  }
+        return questionMapper.questionToQuestionReadDTO(savedQuestion, user);
+    }
 
-  public QuestionReadDTO updateQuestion(QuestionUpdateDTO questionUpdateDTO) {
-    Long existingQuestionId = questionUpdateDTO.getId();
-    Question existingQuestion = questionRepository.findById(existingQuestionId).orElseThrow();
+    public QuestionReadDTO updateQuestion(QuestionUpdateDTO questionUpdateDTO) {
+        Long existingQuestionId = questionUpdateDTO.getId();
+        Question existingQuestion = questionRepository.findById(existingQuestionId).orElseThrow();
 
-    existingQuestion.updateContent(questionUpdateDTO.getContent());
-    questionRepository.save(existingQuestion);
+        existingQuestion.updateContent(questionUpdateDTO.getContent());
+        questionRepository.save(existingQuestion);
 
-    return questionMapper.questionToQuestionReadDTO(existingQuestion, existingQuestion.getUser());
-  }
+        return questionMapper.questionToQuestionReadDTO(existingQuestion,
+            existingQuestion.getUser());
+    }
 
-  public void deleteQuestion(Long id) {
-    questionRepository.deleteById(id);
-  }
+    public void deleteQuestion(Long id) {
+        questionRepository.deleteById(id);
+    }
 
-  public QuestionReadDTO completeQuestion(QuestionToggleSolvedDTO questionToggleSolvedDTO) {
-    Question existingQuestion = questionRepository.findById(questionToggleSolvedDTO.getId())
-        .orElseThrow();
+    public QuestionReadDTO completeQuestion(QuestionToggleSolvedDTO questionToggleSolvedDTO) {
+        Question existingQuestion = questionRepository.findById(questionToggleSolvedDTO.getId())
+            .orElseThrow();
 
-    existingQuestion.toggleSolved(questionToggleSolvedDTO.isSolved());
-    Question updatedQuestion = questionRepository.save(existingQuestion);
+        existingQuestion.toggleSolved(questionToggleSolvedDTO.isSolved());
+        Question updatedQuestion = questionRepository.save(existingQuestion);
 
-    return questionMapper.questionToQuestionReadDTO(updatedQuestion, updatedQuestion.getUser());
-  }
+        return questionMapper.questionToQuestionReadDTO(updatedQuestion, updatedQuestion.getUser());
+    }
 
-  public QuestionReadDTO recommendQuestion(
-      QuestionToggleRecommendedDTO questionToggleRecommendedDTO) {
-    Question existingQuestion = questionRepository.findById(questionToggleRecommendedDTO.getId())
-        .orElseThrow();
+    public QuestionReadDTO recommendQuestion(
+        QuestionToggleRecommendedDTO questionToggleRecommendedDTO) {
+        Question existingQuestion = questionRepository.findById(
+                questionToggleRecommendedDTO.getId())
+            .orElseThrow();
 
-    existingQuestion.toggleRecommended(questionToggleRecommendedDTO.isRecommended());
-    questionRepository.save(existingQuestion);
+        existingQuestion.toggleRecommended(questionToggleRecommendedDTO.isRecommended());
+        questionRepository.save(existingQuestion);
 
-    return questionMapper.questionToQuestionReadDTO(existingQuestion, existingQuestion.getUser());
-  }
+        return questionMapper.questionToQuestionReadDTO(existingQuestion,
+            existingQuestion.getUser());
+    }
 
-  public Page<QuestionReadDTO> getPageQuestions(Pageable pageable) {
-    return questionRepository.findAllQuestionReadDTOs(pageable);  // 질문 목록을 페이지네이션으로 조회
-  }
+    public Page<QuestionReadDTO> getPageQuestions(Pageable pageable) {
+        return questionRepository.findAllQuestionReadDTOs(pageable);  // 질문 목록을 페이지네이션으로 조회
+    }
 }
