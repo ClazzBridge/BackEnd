deeb176de460524870c76568e954740a33c43ca2
Bulgogi-Pizza <quitendexit@gmail.com>
2024-10-08 11:20:31 +0900

    [#8] fix : studentStatusMapper에서 memberId, isHandRaised, isUnderstanding 매핑 버그 수정

--- src/main/java/com/example/academy/dto/StudentStatusReadDTO.java
+++ src/main/java/com/example/academy/dto/StudentStatusReadDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StudentStatusReadDTO {
 
-  private Long userId;
+  private Long memberId;
   private boolean isHandRaised;
   private boolean isUnderstanding;
 }
--- src/main/java/com/example/academy/mapper/StudentStatusMapper.java
+++ src/main/java/com/example/academy/mapper/StudentStatusMapper.java
@@ -3,6 +3,7 @@ package com.example.academy.mapper;
 import com.example.academy.domain.StudentStatus;
 import com.example.academy.dto.StudentStatusReadDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,8 @@ public interface StudentStatusMapper {
 
   StudentStatusMapper INSTANCE = Mappers.getMapper(StudentStatusMapper.class);
 
+  @Mapping(source = "member.id", target = "memberId")
+  @Mapping(source = "handRaised", target = "isHandRaised")
+  @Mapping(source = "understanding", target = "isUnderstanding")
   StudentStatusReadDTO studentStatusToStudentStatusReadDTO(StudentStatus studentStatus);
 }
