8318d1cdc5eed663df0382cb0a60a6ce916f5bd8
ftfg777 <ftfgz5093@gmail.com>
2024-10-10 16:35:59 +0900

    .

--- src/main/java/com/example/academy/dto/auth/LoginResponseDTO.java
+++ src/main/java/com/example/academy/dto/auth/LoginResponseDTO.java
@@ -9,13 +9,21 @@ import lombok.Setter;
 @Setter
 public class LoginResponseDTO {
 
-  private String accessToken;
-  private Cookie refreshTokenCookie;
+    private String accessToken;
+    private Cookie refreshTokenCookie;
+    private AuthResponseDTO authResponseDTO;
 
-  public LoginResponseDTO(String accessToken, Cookie refreshTokenCookie) {
-    this.accessToken = accessToken;
-    this.refreshTokenCookie = refreshTokenCookie;
-  }
+    public LoginResponseDTO(String accessToken, Cookie refreshTokenCookie) {
+        this.accessToken = accessToken;
+        this.refreshTokenCookie = refreshTokenCookie;
+    }
 
-  // Getter/Setter 생략
+    public LoginResponseDTO(String accessToken, Cookie refreshTokenCookie,
+        AuthResponseDTO authResponseDTO) {
+        this.accessToken = accessToken;
+        this.refreshTokenCookie = refreshTokenCookie;
+        this.authResponseDTO = authResponseDTO;
+    }
+
+    // Getter/Setter 생략
 }
