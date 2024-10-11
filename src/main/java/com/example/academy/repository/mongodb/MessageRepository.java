f816a8e25cb012276e8da4d0601f2b6f9cabebd0
seulk <skbb11@naver.com>
2024-10-11 19:26:56 +0900

    fix:로그인

--- src/main/java/com/example/academy/repository/mongodb/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.academy.repository.mongodb;
-
-import com.example.academy.domain.mongodb.Message;
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageRepository extends MongoRepository<Message, String> {
-  List<Message> findByChatRoomId(String chatRoomId);
-}
--- src/main/java/com/example/academy/repository/mysql/ProfileImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.academy.repository.mysql;
-
-import com.example.academy.domain.mysql.AvatarImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImageRepository extends JpaRepository<AvatarImage, Long> {
-
-
-}
