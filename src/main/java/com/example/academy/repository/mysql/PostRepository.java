d4fd003987313576de9f3cb887a751694ea70414
Bulgogi-Pizza <quitendexit@gmail.com>
2024-10-11 20:10:20 +0900

    [#3] fix : DB변경으로 인한 기능 변경 완료

--- src/main/java/com/example/academy/repository/mysql/PostRepository.java
+++ src/main/java/com/example/academy/repository/mysql/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.academy.repository.mysql;
 
+import com.example.academy.domain.mysql.Post;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
