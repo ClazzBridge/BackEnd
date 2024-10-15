c22f5c8f6618fe47317898659963bec1dc485bfe
Bulgogi-Pizza <quitendexit@gmail.com>
2024-10-15 18:36:52 +0900

    commit for merge

--- src/main/java/com/example/academy/repository/mongodb/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.academy.repository.mongodb;
-
-import com.example.academy.domain.mongodb.ChatRoom;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
-
-}
