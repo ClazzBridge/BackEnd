5d2ec33e072050b5657334c353894d35f599e31e
Seulki Kang <skbb11@naver.com>
2024-10-21 16:09:53 +0900

    fix:예외처리

--- src/main/java/com/example/academy/controller/CourseController.java
+++ src/main/java/com/example/academy/controller/CourseController.java
@@ -66,7 +66,7 @@ public class CourseController {
     }
   }
 
-  @PutMapping
+  @PutMapping()
   @Operation(summary = "강의 변경", security = {@SecurityRequirement(name = "bearerAuth")})
   public ResponseEntity<?> updateCourse(@RequestBody CourseUpdateDTO courseUpdateDTO) {
     try {
