0e93172840e1f9b6e66703600d646b6914eca02c
Seulki Kang <skbb11@naver.com>
2024-10-25 17:00:35 +0900

    a

--- src/main/java/com/example/academy/config/WebConfig.java
+++ src/main/java/com/example/academy/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000") // 프론트엔드 URL
+        .allowedOrigins("http://clazz-bridge-service.default.svc.cluster.local:3000") // 프론트엔드 URL
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*") // Authorization 헤더 허용
         .allowCredentials(true);
